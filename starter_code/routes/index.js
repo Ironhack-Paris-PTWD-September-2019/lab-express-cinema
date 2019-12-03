6c84e7d3a618a36967afa36051a799636a721e81
Elisa Salamanca <e_l_salamanca@yahoo.fr>
2019-12-03 22:31:15 +0100

    Iteration 2

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router  = express.Router();
 
+const Movie = require('../models/Movie.js'); 
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+/* GET movies page */ 
+
+router.get('/movies', (req, res, next) => {
+  Movie.find().then(function(movies){
+    console.log('Movies are loaded');
+    res.render('movies', {
+      movies : movies 
+    });
+    
+  })
+  .catch(err=>console.error(err));
+  
+});
+
 
 module.exports = router;
