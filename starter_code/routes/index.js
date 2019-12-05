a48c12214b6d14ba9f9327f4b63986ff6a179559
Alizee Jan <jan.alizee@gmail.com>
2019-12-05 18:03:58 +0100

    iteration 3 error

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
 
+const Movie = require('../models/Movie.js');
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(allTheMoviesFromDB => {
+      res.render('movies', { movies: allTheMoviesFromDB });
+    })
+    .catch(error => {
+      console.log('Error while getting the movies from the DB: ', error);
+    })
+});
+
 
 module.exports = router;
