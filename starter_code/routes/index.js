e6e77d319542f45141569acf016e5991f03a5ec7
Thao Nhu <thaonhu2710@yahoo.com>
2019-12-05 23:05:40 +0100

    1st commit

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find().then(function(movies){
+    console.log("Mes films de la DB sont",movies)
+    res.render('movies', {
+      movies:movies
+    });
+  }).catch( err => console.error(err));
+  
+
+});
+
+router.get('/movies/:id',function(req,res,next){
+  Movie.find({_id:req.params.id}).then(function(movies){
+    let movie = movies [0];
+
+    res.render("moviedetails", {
+      movie:movie
+    })
+  }).catch(err => console.error(err))
+  
+})
 
 module.exports = router;
