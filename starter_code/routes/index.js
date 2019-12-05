9faa43ec00b924807e1c20f87ceb42516c9873c2
Sofy-Prd <sofy@DESKTOP-4LRCARK.localdomain>
2019-12-05 20:17:56 +0100

    final commit

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router  = express.Router();
+const Movie= require('../models/Movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
+  
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find().then(function(movies){
+    console.log('les films de la DB sont :', movies);
+    res.render('movies', {
+      movies:movies
+    });
+
+  }).catch(err=>console.error(err))
+ 
+});
+
+router.get('/movies/:movieid', function(req, res, next){
+  Movie.find({_id:req.params.movieid}).then(function(movie) {
+    
+    res.render('MovieDetails', {
+      movie:movie[0]
+      
+    });
+  }).catch(err=>console.error(err))
+  
 });
 
 module.exports = router;
