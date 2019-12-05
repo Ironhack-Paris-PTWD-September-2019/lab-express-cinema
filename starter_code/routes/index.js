4c4f160dd492c6553e1583231d1ab99b1a5d4145
Enelyah <ms.jen.christine@gmail.com>
2019-12-05 22:12:31 +0100

    jenny

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router  = express.Router();
+const movie = require( "../models/movie" );
 
-/* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  movie.find()
+        .then(movie => res.render('movies', {movie}))
+        .catch(error => console.log('Error while getting movies from the DB: ', error))
+});
+
+router.get('/movies/:movieId', (req, res, next) => {
+  movie.findOne({'_id': req.params.movieId})
+    .then(movie => {
+      res.render('movie-details', {movie});
+    })
+    .catch(error => {
+      console.log('Error while retrieving movie details: ', error);
+    })
 });
 
 module.exports = router;
