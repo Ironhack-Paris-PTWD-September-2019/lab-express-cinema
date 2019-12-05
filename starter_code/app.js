cb211d4e5bf982f8bf7a1281d2e2fde3c3f174a0
blablamedia <pierre@gambaud.com>
2019-12-05 22:16:27 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/cinema', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -43,15 +43,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Default title';
 
 
 
-const index = require('./routes/index');
+const index = require('./routes/index.js');
 app.use('/', index);
 
 
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router  = express.Router();
+const Movies = require(`../models/Movies.js`);
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+/* GET movies list page */
+router.get('/movies', (req, res, next) => {
+  Movies.find()
+    .then(movies => {
+      console.log(movies);
+      res.render('movies', {
+        movies: movies
+      });
+    })
+    .catch(err => next(err))
+});
+
+/* GET movie details page */
+router.get(`/movie/:id`, (req, res, next) => {
+  Movies.findOne({"_id": req.params.id})
+    .then(movie => {
+      console.log(movie);
+      res.render(`movie`, {
+          movie: movie
+      });
+    })
+    .catch(err => next(err))
+})
+
 
 module.exports = router;
