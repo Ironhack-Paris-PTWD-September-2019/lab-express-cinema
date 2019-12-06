b4573286f93dc3de8f3e3bc066c97daf541e44b2
Michel Ho <mho85@icloud.com>
2019-12-06 18:03:31 +0100

    Iteration 3 done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Cinema Ironhack' });
 });
+
+router.get('/movies', (req, res, next) => {
+
+  Movie
+    .find()
+    .then(movies => {
+      res.render('movies',
+        {
+          title: 'Cinema Ironhack',
+          movies: movies
+        })
+    })
+    .catch(e => console.error(e))
+
+})
 
 module.exports = router;
