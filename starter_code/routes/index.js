ac473b0a3dff245b5a1e47d729313605e1b689fd
Sandrine8304 <sandrine.auberval@gmail.com>
2019-12-06 17:05:54 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+/* GET movies page */
 router.get('/movies', (req, res, next) => {
-  res.render('movies');
+  Movie.find()
+  .then(allMoviesFromDB => {
+    res.render('movies', {movies: allMoviesFromDB});
+  })
+  .catch(error => {
+    console.log('Error while getting the movies from the DB:', error);
+  })
 });
 
+/* GET details page */
+router.get('/movies/:id', (req, res, next) => {
+  Movie.findOne({'_id': req.params.id})
+    .then(theMovie => {
+      res.render('movie-details', { movie: theMovie });
+    })
+    .catch(error => {
+      console.log('Error while retrieving the book details: ', error);
+    })
+});
 
 module.exports = router;
