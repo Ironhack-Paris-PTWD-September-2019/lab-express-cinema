fd996ace2b5784e283325c8f3627049732cc4779
Douha <douhadal9@gmail.com>
2019-12-11 19:46:18 +0100

    Douha et Audrey

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/movies'); 
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies',(req, res, next)=>{
+  Movie.find().then(function(moviesFromDb){
+    console.log('movies',moviesFromDb)
+    res.render('movies', {
+      allMovies: moviesFromDb,
+    
+    });
+  }).catch(err =>console.error(err));
+});
+
+router.get('/movies/:id', function(req, res, next){
+  Movie.findById(req.params.id)
+  .then(theMovie =>{
+    res.render('movie-details',{movie : theMovie})
+  })
+.catch(err =>console.error(err));
+});
+
 
 module.exports = router;
